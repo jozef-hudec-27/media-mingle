897dfe19f48c9c989611156dff983bccae6560b6
Jozef Hudec <jojohudec27@gmail.com>
2023-04-12 06:22:48 +0200

    Use PostgreSQL instead of SQLite

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,8 +20,8 @@ module MediaMingle
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g|
-      g.template_engine nil #to skip views
-      g.test_framework  nil #to skip test framework
+      g.template_engine nil # to skip views
+      g.test_framework  nil # to skip test framework
       g.assets false
       g.helper false
       g.stylesheets false
