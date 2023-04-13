a3c20ee0c427924bb0252ec34850f73d6778e3b4
Jozef Hudec <jojohudec27@gmail.com>
2023-04-13 05:39:52 +0200

    Handle creating users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :lockable
+
+  validates :username, presence: true, length: { maximum: 100 }
 end
