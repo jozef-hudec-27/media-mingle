2b03017a28711755e15247013ffef2ec923dd5e8
Jozef Hudec <jojohudec27@gmail.com>
2023-04-17 06:17:48 +0200

    Handle non existent routes in React

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './general/Navbar'
 import Home from './pages/Home'
+import PageNotFound from './pages/PageNotFound'
 
 export default function App() {
   return (
@@ -10,6 +11,8 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
   )
