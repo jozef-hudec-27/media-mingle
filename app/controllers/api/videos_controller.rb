e7114e8908607c73fef76ea16216ba523012f97c
Jozef Hudec <jojohudec27@gmail.com>
2023-04-25 18:35:00 +0200

    Add styles to video preview

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class Api::VideosController < ApplicationController
   def index
     videos = Video.all
@@ -6,10 +8,30 @@ class Api::VideosController < ApplicationController
                             video_url: url_for(video.video),
                             thumbnail_url: url_for(video.thumbnail),
                             view_count: video.views.count,
+                            duration: format_video_duration(video_duration(video)),
                             user: video.user
                           })
     end
 
     render json: videos
+  end
+
+  private
+
+  # returns duration of a video in seconds
+  def video_duration(video)
+    FFMPEG::Movie.new(url_for(video.video)).duration.round
+  end
+
+  # returns duration in the format 'hh:mm:ss'
+  def format_video_duration(duration_sec)
+    hours = duration_sec / 3600
+    minutes = (duration_sec / 60) % 60
+    seconds = duration_sec % 60
+    if hours.positive?
+      "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+    else
+      "#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+    end
   end
 end
--- app/javascript/components/video/VideoPreview.js
+++ app/javascript/components/video/VideoPreview.js
@@ -3,9 +3,18 @@ import { timeElapsedSince } from '../../utils'
 
 export default function VideoPreview({ video }) {
   return (
-    <div className="video-preview flexbox flex-column gap-12">
-      <a href={video.video_url}>
+    <div className="video-preview flexbox flex-column gap-12" onClick={(e) => {
+      if (e.target.tagName !== 'A') {
+        // If we're not clicking a link
+        console.log('DENIS')
+      }
+    }}>
+      <a className="thumbnail-wrapper" href={video.video_url}>
         <img className="thumbnail" src={video.thumbnail_url} alt="Video thumbnail" />
+
+        <div className="video-duration-box p-2">
+          <p>{video.duration}</p>
+        </div>
       </a>
 
       <div className="flexbox gap-12">
@@ -19,7 +28,7 @@ export default function VideoPreview({ video }) {
           </h3>
 
           <div className="video-info">
-            <a href={video.video_url}>{video.user.username}</a>
+            <a className="author" href={video.video_url}>{video.user.username}</a>
 
             <p>
               {video.view_count} views • {timeElapsedSince(new Date(video.created_at))}
